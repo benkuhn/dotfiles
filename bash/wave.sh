97b183ef4727d5d4776df7b4c760d4e0e7e998d1
Ben Kuhn <ben.s.kuhn@gmail.com>
2017-03-29 10:48:48 +0000

    Add Wave config

---
+++ bash/wave.sh
@@ -0,0 +1,6 @@
+export LC_CTYPE=en_US.UTF-8
+export PGDATA=/usr/local/var/postgres
+export PGHOST=localhost
+export WORKON_HOME=~/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+source /usr/local/bin/virtualenvwrapper.sh
