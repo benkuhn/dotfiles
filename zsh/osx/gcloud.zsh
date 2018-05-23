17549de9d3817e869b6f4b369e4147795767825c
Ben Kuhn <ben.s.kuhn@gmail.com>
2018-05-23 19:56:22 +0000

    Gcloud and git fixes

--- zsh/osx/gcloud.zsh
+++ zsh/osx/gcloud.zsh
@@ -1,9 +1,10 @@
-local GCLOUD_DIR=/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/
+local GCLOUD_DIR=$HOME/bin/google-cloud-sdk
 if [ -d $GCLOUD_DIR ]; then
     # The next line updates PATH for the Google Cloud SDK.
     source "$GCLOUD_DIR/path.zsh.inc"
     # The next line enables shell command completion for gcloud.
     source "$GCLOUD_DIR/completion.zsh.inc"
+    export CLOUDSDK_PYTHON=$(which python2.7)
     myconfigs[gcloud]=installed
 else
     myconfigs[gcloud]=not-installed
