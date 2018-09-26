ea83a9602f11c1326d0a5ccc08acab01c337588c
Ben Kuhn <ben.s.kuhn@gmail.com>
2018-09-26 22:21:35 +0000

    Use locals in window.zsh

--- zsh/osx/window.zsh
+++ zsh/osx/window.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 not_focused () {
-    _FRONT=$(osascript -e 'tell application "System Events" to return name of first application process whose frontmost is true')
+    local _FRONT=$(osascript -e 'tell application "System Events" to return name of first application process whose frontmost is true')
     if [[ $_FRONT != "iTerm2" ]]; then
         return 0
     fi
-    _FRONT_TTY=$(osascript - <<EOF
+    local _FRONT_TTY=$(osascript - <<EOF
 tell application "iTerm2"
      tell current session of current tab of current window
           return tty
@@ -21,7 +21,7 @@ EOF
 
 send_notification () {
     if exists terminal-notifier; then
-        APPID=com.googlecode.iTerm2
+        local APPID=com.googlecode.iTerm2
         terminal-notifier -message "$2" -title "$1" -activate $APPID -sender $APPID
     else
         osascript -e "display notification \"$2\" with title \"$1\""
