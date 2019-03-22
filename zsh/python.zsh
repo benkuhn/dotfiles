3c4e38cba1eeec5d1674ada018c62dc449d1fc21
Ben Kuhn <ben.s.kuhn@gmail.com>
2019-03-22 16:27:40 -0400

    Pyenv hook

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 local PYUTILS_DIR=$HOME/code/pyutils/
-if [ -d $PYUTILS_DIR ]; then
+if [[ -d $PYUTILS_DIR ]]; then
     pythonpath+="$PYUTILS_DIR"
     myconfigs[python-pyutils]=installed
 else
@@ -8,10 +8,17 @@ else
 fi
 
 local VIRTUALENVWRAPPER_PATH=/usr/local/bin/virtualenvwrapper_lazy.sh
-if [ -x $VIRTUALENVWRAPPER_PATH ]; then
+if [[ -x $VIRTUALENVWRAPPER_PATH ]]; then
     VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
     source $VIRTUALENVWRAPPER_PATH
     myconfigs[python-virtualenvwrapper]=installed
 else
     myconfigs[python-virtualenvwrapper]=not-installed
+fi
+
+if which pyenv > /dev/null && [[ -d ~/.pyenv ]] ; then
+    path+=~/.pyenv/shims
+    myconfigs[python-pyenv]=installed
+else
+    myconfigs[python-pyenv]=not-installed
 fi
