7ffc608376a060c2e4b196beb081791516381a77
Ben Kuhn <ben.s.kuhn@gmail.com>
2023-01-21 18:41:45 -0500

    Add alternate homebrew location

--- zsh/osx/homebrew.zsh
+++ zsh/osx/homebrew.zsh
@@ -2,9 +2,13 @@ if [ -d /usr/local/bin ]; then
     # allow Homebrew bins to shadow system bins and manpages
     path+=(/usr/local/bin /usr/local/sbin)
     myconfigs[homebrew]=installed
+elif [ -d /opt/homebrew/bin ]; then
+    path+=(/opt/homebrew/bin)
+    myconfigs[homebrew]=installed
 else
     myconfigs[homebrew]=not-installed
 fi
+
 # Use GNU coreutils because they're better than OS X's
 local GNUBIN_PATH=/usr/local/opt/coreutils/libexec/gnubin
 if [ -d $GNUBIN_PATH ]; then
